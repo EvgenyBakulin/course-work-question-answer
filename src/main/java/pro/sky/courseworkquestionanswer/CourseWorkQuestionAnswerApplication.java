0c06ac249cf2272b4ad269d43653d0afbb9635e5
Евгений <ugend84@yandex.ru>
2022-05-15 14:59:29 +0300

    Initial commit

---
+++ src/main/java/pro/sky/courseworkquestionanswer/CourseWorkQuestionAnswerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworkquestionanswer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkQuestionAnswerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkQuestionAnswerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/courseworkquestionanswer/CourseWorkQuestionAnswerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworkquestionanswer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkQuestionAnswerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
